37b16ea00855e7e5200adb589dfc2d70efae74b8
satendra gangwar <gsatendra488@gmail.com>
2023-12-27 22:48:04 +0530

    first-commit

---
+++ src/sections/products/product-card.jsx
@@ -0,0 +1,97 @@
+
+import PropTypes from 'prop-types';
+
+import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
+import Card from '@mui/material/Card';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+
+import { fCurrency } from 'src/utils/format-number';
+
+import Label from 'src/components/label';
+import { ColorPreview } from 'src/components/color-utils';
+
+// ----------------------------------------------------------------------
+
+export default function ShopProductCard({ product }) {
+  const renderStatus = (
+    <Label
+      variant="filled"
+      color={(product.status === 'sale' && 'error') || 'info'}
+      sx={{
+        zIndex: 9,
+        top: 16,
+        right: 16,
+        position: 'absolute',
+        textTransform: 'uppercase',
+        // box-shadow:'rgba(0,0,0,0.2) 0px 18px 50px -10px',
+        
+      }}
+    >
+      {product.status}
+    </Label>
+  );
+
+  const renderImg = (
+    <Box
+      component="img"
+      alt={product.name}
+      src={product.cover}
+      sx={{
+        top: 0,
+        width: 1,
+        height: 1,
+        objectFit: 'cover',
+        position: 'absolute',
+        
+      }}
+    />
+  );
+
+  const renderPrice = (
+    <Typography variant="subtitle1">
+      <Typography
+        component="span"
+        variant="body1"
+        sx={{
+          color: 'text.disabled',
+          textDecoration: 'line-through',
+        }}
+      >
+        {product.priceSale && fCurrency(product.priceSale)}
+      </Typography>
+      &nbsp;
+      {fCurrency(product.price)}
+    </Typography>
+  );
+
+  return (
+    <Card sx={{fontStyle:'italic', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+    transition: 'transform 0.3s ease-in-out',
+    '&:hover': {
+      transform: 'scale(1.05)',
+    },}}>
+      <Box sx={{ pt: '100%', position: 'relative' ,cursor:'pointer'}}>
+        {product.status && renderStatus}
+
+        {renderImg}
+      </Box>
+
+      <Stack spacing={2} sx={{ p: 3 }}>
+        <Link color="inherit" underline="hover" variant="subtitle2" noWrap>
+          {product.name}
+        </Link>
+
+        <Stack direction="row" alignItems="center" justifyContent="space-between">
+          <ColorPreview colors={product.colors} />
+          {renderPrice}
+        </Stack>
+      </Stack>
+    </Card>
+  );
+}
+
+ShopProductCard.propTypes = {
+  product: PropTypes.object,
+};
